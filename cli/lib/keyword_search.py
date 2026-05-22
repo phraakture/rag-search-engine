2d01d3acc8e5b0b3584c65b3330dcd7119b36606
acegikmoo <rubairakib11@gmail.com>
2026-05-22 21:10:12 +0600

    feat: tokenize

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,14 +1,31 @@
+import string
 from lib.search_utils import load_movies
+
+def clean_text(text):
+    text = text.lower()
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    return text
+
+def tokenize_text(text):
+    text = clean_text(text)
+    tokens = [tok for tok in text.split() if tok]
+    return tokens
+
+def has_matching_token(query_tokens, movie_tokens):
+    for query_tok in query_tokens:
+        for movie_tok in movie_tokens:
+            if query_tok in movie_tok:
+                return True
+    return False
 
 def search_command(query, n_results):
     movies = load_movies()
     res = []
+    query_tokens = tokenize_text(query)
     for movie in movies:
-        if query in movie['title']:
+        movie_tokens = tokenize_text(movie['title'])
+        if has_matching_token(query_tokens, movie_tokens):
             res.append(movie)
         if len(res) == n_results:
             break
     return res
-
-    
-
--- lib/keyword_search.py
+++ lib/keyword_search.py
@@ -1,19 +1,26 @@
 import json
+import string
 from pathlib import Path
 
 DATA_DIR = Path(__file__).resolve().parent.parent / "data"
 MOVIES_PATH = DATA_DIR / "movies.json"
 
+def clean_text(text):
+    text = text.lower()
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    return text
 
-def search_command(query: str, max_results: int = 5) -> list[dict]:
+def search_command(query: str, n_results):
     with open(MOVIES_PATH) as f:
         data = json.load(f)
 
     results = []
+
     for movie in data["movies"]:
-        if query in movie["title"]:
+        if query.lower() in movie["title"].lower():
             results.append(movie)
-        if len(results) >= max_results:
+
+        if len(results) >= n_results:
             break
 
     return results
