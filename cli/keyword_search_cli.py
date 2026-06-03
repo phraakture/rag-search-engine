0cb671471fe6c5836164c41d5acbb4f9f9817680
acegikmoo <rubairakib11@gmail.com>
2026-06-03 20:18:49 +0600

    feat: use index
    Signed-off-by: acegikmoo <rubairakib11@gmail.com>

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -2,14 +2,20 @@
 
 import argparse
 
-from lib.keyword_search import search_command
+from lib.keyword_search import (
+    search_command,
+    build_command,
+    ) 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
+
     search_parser.add_argument("query", type=str, help="Search query")
+
+    search_parser = subparsers.add_parser("build", help="Search movies using BM25")
 
     args = parser.parse_args()
 
@@ -19,6 +25,8 @@ def main() -> None:
             print(f"Searching for: {args.query}")
             for i, result in enumerate(results):
                 print(f"{i + 1}. {result['title']}")
+        case "build":
+            build_command()
         case _:
             parser.print_help()
 
--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,9 +1,56 @@
+import pickle
 import string
-from lib.search_utils import load_movies, load_stopwords
-from lib.search_utils import load_movies, load_stopwords
+from lib.search_utils import CACHE_PATH, load_movies, load_stopwords
 from nltk.stem import PorterStemmer
+from collections import defaultdict
+import os
 
 stemmer = PorterStemmer()
+
+class InvertedIndex:
+    def __init__(self):
+        self.index = defaultdict(set)
+        self.docmap = {}
+        self.index_path = CACHE_PATH / 'index.pkl'
+        self.docmap_path = CACHE_PATH / 'docmap.pkl'
+
+    def __add_document(self, doc_id, text):
+        tokens = tokenize_text(text)
+        for token in set(tokens):
+            self.index[token].add(doc_id)
+
+    def get_documents(self, term):
+        return sorted(list(self.index[term]))
+
+    def build(self):
+        movies = load_movies()
+        for movie in movies:
+            doc_id = movie['id']
+            text = f"{movie['title']} {movie['description']}"
+            self.__add_document(doc_id, text)
+            self.docmap[doc_id] = movie
+
+    def save(self):
+        os.makedirs(CACHE_PATH, exist_ok=True)
+        with open(self.index_path, 'wb') as f:
+            pickle.dump(self.index, f)
+        with open(self.docmap_path, 'wb') as f:
+            pickle.dump(self.docmap, f)
+
+    def load(self):
+        with open(self.index_path, "rb") as f:
+            self.index = pickle.load(f)
+        with open(self.docmap_path, 'rb') as f:
+            self.docmap = pickle.load(f)
+
+
+def build_command():
+    idx = InvertedIndex()
+    idx.build()
+    idx.save()
+    # docs = idx.get_documents("merida")
+    # print(f"First document for token 'merida' = {docs[0]}")
+        
 
 def clean_text(text):
     text = text.lower()
@@ -26,7 +73,6 @@ def tokenize_text(text):
 
     return res
 
-
 def has_matching_token(query_tokens, movie_tokens):
     for query_tok in query_tokens:
         for movie_tok in movie_tokens:
@@ -34,14 +80,22 @@ def has_matching_token(query_tokens, movie_tokens):
                 return True
     return False
 
-def search_command(query, n_results):
+def search_command(query, n_results=5):
     movies = load_movies()
+    idx = InvertedIndex()
+    idx.load()
+    seen, res = set(), []
     res = []
     query_tokens = tokenize_text(query)
-    for movie in movies:
-        movie_tokens = tokenize_text(movie['title'])
-        if has_matching_token(query_tokens, movie_tokens):
-            res.append(movie)
-        if len(res) == n_results:
-            break
+    for qt in query_tokens:
+        matching_doc_ids = idx.get_documents(qt)
+        for matching_doc_id in matching_doc_ids:
+            if matching_doc_id in seen:
+                continue
+            seen.add(matching_doc_id)
+            matching_doc = idx.docmap[matching_doc_id]
+            res.append(matching_doc)
+
+            if len(res) >= n_results:
+                return res
     return res
