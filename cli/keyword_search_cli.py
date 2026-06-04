3afc05ed8242aff1a24c4e0854a8a011f7d0a3e3
acegikmoo <rubairakib11@gmail.com>
2026-06-04 11:36:41 +0600

    feat: tf-idf
    Signed-off-by: acegikmoo <rubairakib11@gmail.com>

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -5,17 +5,36 @@ import argparse
 from lib.keyword_search import (
     search_command,
     build_command,
-    ) 
+    tf_command,
+    idf_command,
+    tfidf_command,
+)
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
-
     search_parser.add_argument("query", type=str, help="Search query")
 
-    search_parser = subparsers.add_parser("build", help="Search movies using BM25")
+    build_parser = subparsers.add_parser("build", help="Build inverted index")
+    build_parser.set_defaults(func=lambda: build_command())
+
+    tf_parser = subparsers.add_parser("tf", help="Get term frequency for a document")
+    tf_parser.add_argument("doc_id", type=str, help="Document ID")
+    tf_parser.add_argument("term", type=str, help="Term to find frequency for")
+
+    idf_parser = subparsers.add_parser(
+        "idf", help="Calculate inverse document frequency"
+    )
+    idf_parser.add_argument("term", type=str, help="Search term to find idf for")
+
+    tfidf_parser = subparsers.add_parser(
+        "tfidf", help="Get TF-IDF for a document and term"
+    )
+    tfidf_parser.add_argument("doc_id", type=str, help="Document ID")
+    tfidf_parser.add_argument("term", type=str, help="Term to find frequency for")
 
     args = parser.parse_args()
 
@@ -27,9 +46,15 @@ def main() -> None:
                 print(f"{i + 1}. {result['title']}")
         case "build":
             build_command()
+        case "tf":
+            tf_command(args.doc_id, args.term)
+        case "idf":
+            idf_command(args.term)
+        case "tfidf":
+            tfidf_command(args.doc_id, args.term)
         case _:
             parser.print_help()
 
+
 if __name__ == "__main__":
     main()
-
--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -2,65 +2,115 @@ import pickle
 import string
 from lib.search_utils import CACHE_PATH, load_movies, load_stopwords
 from nltk.stem import PorterStemmer
-from collections import defaultdict
+from collections import Counter, defaultdict
 import os
+import math
 
 stemmer = PorterStemmer()
+
 
 class InvertedIndex:
     def __init__(self):
         self.index = defaultdict(set)
         self.docmap = {}
-        self.index_path = CACHE_PATH / 'index.pkl'
-        self.docmap_path = CACHE_PATH / 'docmap.pkl'
+        self.index_path = CACHE_PATH / "index.pkl"
+        self.docmap_path = CACHE_PATH / "docmap.pkl"
+        self.term_frequencies_path = CACHE_PATH / "term_frequencies.pkl"
+        self.term_frequencies = defaultdict(Counter)
 
     def __add_document(self, doc_id, text):
         tokens = tokenize_text(text)
         for token in set(tokens):
             self.index[token].add(doc_id)
+        self.term_frequencies[doc_id].update(tokens)
 
     def get_documents(self, term):
         return sorted(list(self.index[term]))
 
+    def get_tf(self, doc_id, term):
+        token = tokenize_text(term)
+        if len(token) != 1:
+            raise ValueError("can only have 1 tokens")
+        doc_id = int(doc_id) if not isinstance(doc_id, int) else doc_id
+        return self.term_frequencies[doc_id][token[0]]
+
+    def get_idf(self, term):
+        token = tokenize_text(term)
+        if len(token) != 1:
+            raise ValueError("can only have 1 tokens")
+        token = token[0]
+        doc_count = len(self.docmap)
+        term_doc_count = len(self.index[token])
+        return math.log((doc_count + 1) / (term_doc_count + 1))
+
+    def get_tfidf(self, doc_id, term):
+        tf = self.get_tf(doc_id, term)
+        idf = self.get_idf(term)
+        return tf * idf
+
     def build(self):
         movies = load_movies()
         for movie in movies:
-            doc_id = movie['id']
+            doc_id = movie["id"]
             text = f"{movie['title']} {movie['description']}"
             self.__add_document(doc_id, text)
             self.docmap[doc_id] = movie
 
     def save(self):
         os.makedirs(CACHE_PATH, exist_ok=True)
-        with open(self.index_path, 'wb') as f:
+        with open(self.index_path, "wb") as f:
             pickle.dump(self.index, f)
-        with open(self.docmap_path, 'wb') as f:
+        with open(self.docmap_path, "wb") as f:
             pickle.dump(self.docmap, f)
+        with open(self.term_frequencies_path, "wb") as f:
+            pickle.dump(self.term_frequencies, f)
 
     def load(self):
         with open(self.index_path, "rb") as f:
             self.index = pickle.load(f)
-        with open(self.docmap_path, 'rb') as f:
+        with open(self.docmap_path, "rb") as f:
             self.docmap = pickle.load(f)
+        with open(self.term_frequencies_path, "rb") as f:
+            self.term_frequencies = pickle.load(f)
+
+
+def tfidf_command(doc_id, term):
+    idx = InvertedIndex()
+    idx.load()
+    tf_idf = idx.get_tfidf(doc_id, term)
+    print(f"Inverse document frequency of '{term}': {tf_idf:.2f}")
+
+
+def idf_command(term):
+    idx = InvertedIndex()
+    idx.load()
+    idf = idx.get_idf(term)
+    print(f"Inverse document frequency of '{term}': {idf:.2f}")
+
+
+def tf_command(doc_id, term):
+    idx = InvertedIndex()
+    idx.load()
+    print(idx.get_tf(doc_id, term))
 
 
 def build_command():
     idx = InvertedIndex()
     idx.build()
     idx.save()
-    # docs = idx.get_documents("merida")
-    # print(f"First document for token 'merida' = {docs[0]}")
-        
+
 
 def clean_text(text):
     text = text.lower()
     text = text.translate(str.maketrans("", "", string.punctuation))
     return text
 
+
 def tokenize_text(text):
     text = clean_text(text)
     stopwords = load_stopwords()
     res = []
+
     def _filter(tok):
         if tok and tok not in stopwords:
             return True
@@ -73,6 +123,7 @@ def tokenize_text(text):
 
     return res
 
+
 def has_matching_token(query_tokens, movie_tokens):
     for query_tok in query_tokens:
         for movie_tok in movie_tokens:
@@ -80,12 +131,12 @@ def has_matching_token(query_tokens, movie_tokens):
                 return True
     return False
 
+
 def search_command(query, n_results=5):
     movies = load_movies()
     idx = InvertedIndex()
     idx.load()
     seen, res = set(), []
-    res = []
     query_tokens = tokenize_text(query)
     for qt in query_tokens:
         matching_doc_ids = idx.get_documents(qt)
